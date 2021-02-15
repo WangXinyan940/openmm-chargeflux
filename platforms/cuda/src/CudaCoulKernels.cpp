9a68f6f22c8d18ecef9c2b09c6f51d42c31514bd
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2021-02-15 16:34:33 +0800

    Update CudaCoulKernels.cpp

--- platforms/cuda/src/CudaCoulKernels.cpp
+++ platforms/cuda/src/CudaCoulKernels.cpp
@@ -336,13 +336,13 @@ double CudaCalcCoulForceKernel::execute(ContextImpl& context, bool includeForces
             &numParticles
         };
         cu.executeKernel(indexAtomKernel, argSwitch, numParticles);
-        
+        void* argUpdateCharge[] = {
+            &realcharges_cu.getDevicePointer(),
+            &charges_cu.getDevicePointer()
+        };
+        cu.executeKernel(copyChargeKernel, argUpdateCharge, numParticles);
+
         if (numFluxAngles + numFluxBonds > 0){
-            void* argUpdateCharge[] = {
-                &realcharges_cu.getDevicePointer(),
-                &charges_cu.getDevicePointer()
-            };
-            cu.executeKernel(copyChargeKernel, argUpdateCharge, numParticles);
             void* args_realc[] = {
                 &realcharges_cu.getDevicePointer(),
                 &cu.getPosq().getDevicePointer(),
